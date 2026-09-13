8d14902d9ea83f765dcd8e1f751b08d9f369ebbc
Leo Ring <leoring03@gmail.com>
2026-09-13 14:41:00 +0100

    Added little proof of concept for rendering the pdf output

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,13 @@ use relm4::{
     Component, ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent,
     actions::{AccelsPlus, RelmAction, RelmActionGroup},
     adw,
-    gtk::{self, gio::prelude::FileExt, prelude::ButtonExt},
+    gtk::{
+        self, cairo,
+        gdk::{self, Texture},
+        gio::{Cancellable, prelude::FileExt},
+        glib::object::Cast,
+        prelude::ButtonExt,
+    },
     main_application,
     prelude::FactoryVecDeque,
 };
@@ -23,6 +29,7 @@ pub(super) struct App {
     pub input_files: Vec<PathBuf>,
     pub input_files_widgets: FactoryVecDeque<InputFileWidget>,
     pub output_file: Option<PathBuf>,
+    pub pdf_preview: Option<Texture>,
 }
 
 #[derive(Debug)]
@@ -30,7 +37,7 @@ pub enum AppMsg {
     Quit,
     PickedOutputFile(PathBuf),
     ExportDocument,
-    ExportComplete,
+    UpdatePdfPreview(PathBuf),
     OpenInputFilePicker,
     AddInputFiles(Vec<PathBuf>),
     PickOutputFile,
@@ -122,6 +129,10 @@ impl SimpleComponent for App {
                         gtk::Label{
                             #[watch]
                             set_label: &model.output_file.as_ref().map(|file| file.to_string_lossy().to_string()).unwrap_or_default(),
+                        },
+                        gtk::Picture {
+                            #[watch]
+                            set_paintable: model.pdf_preview.as_ref(),
                         }
                     }
                 }
@@ -142,6 +153,7 @@ impl SimpleComponent for App {
             input_files: vec![],
             input_files_widgets,
             output_file: None,
+            pdf_preview: None,
         };
         let input_files_list_box = model.input_files_widgets.widget();
         let widgets = view_output!();
@@ -183,7 +195,18 @@ impl SimpleComponent for App {
     fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
         match message {
             AppMsg::Quit => main_application().quit(),
-            AppMsg::ExportComplete => {}
+            AppMsg::UpdatePdfPreview(pdf_path) => {
+                let cancellable: Option<&Cancellable> = None;
+                let pdf_doc = poppler::Document::from_gfile(
+                    &gio::File::for_path(pdf_path.to_str().unwrap()),
+                    None,
+                    cancellable,
+                )
+                .unwrap();
+                if let Some(page) = pdf_doc.page(0) {
+                    self.pdf_preview = render_page_to_texture(&page, 150.0);
+                }
+            }
             AppMsg::OpenInputFilePicker => {
                 let sender = sender.clone();
 
@@ -252,10 +275,10 @@ impl SimpleComponent for App {
                         pandoc.add_input(&input);
                     }
                     pandoc.add_option(PandocOption::PdfEngine(PathBuf::from("tectonic")));
-                    pandoc.set_output(OutputKind::File(output_path));
+                    pandoc.set_output(OutputKind::File(output_path.clone()));
                     pandoc.set_show_cmdline(true);
                     pandoc.execute().unwrap();
-                    sender.input(AppMsg::ExportComplete);
+                    sender.input(AppMsg::UpdatePdfPreview(output_path));
                 });
             }
         }
@@ -292,4 +315,39 @@ impl AppWidgets {
             self.main_window.maximize();
         }
     }
+}
+
+fn render_page_to_texture(page: &poppler::Page, dpi: f64) -> Option<gdk::Texture> {
+    let (w_pt, h_pt) = page.size();
+    let scale = dpi / 72.0;
+    let width = (w_pt * scale).ceil() as i32;
+    let height = (h_pt * scale).ceil() as i32;
+
+    let mut cairo_image_surface =
+        cairo::ImageSurface::create(cairo::Format::ARgb32, width, height).ok()?;
+    let cairo_context = cairo::Context::new(&cairo_image_surface).ok()?;
+
+    // Paint white for underneath transparency
+    cairo_context.set_source_rgb(1.0, 1.0, 1.0);
+    cairo_context.paint().ok()?;
+
+    cairo_context.scale(scale, scale);
+    page.render(&cairo_context);
+    drop(cairo_context); // release the Context's reference otherwise data() call will fail
+
+    cairo_image_surface.flush();
+    let stride = cairo_image_surface.stride() as usize;
+    let data = cairo_image_surface.data().ok()?;
+    let bytes = glib::Bytes::from(&data[..]);
+
+    Some(
+        gdk::MemoryTexture::new(
+            width,
+            height,
+            gdk::MemoryFormat::B8g8r8a8Premultiplied,
+            &bytes,
+            stride,
+        )
+        .upcast(),
+    )
 }
